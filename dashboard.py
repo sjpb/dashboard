459e5cb0102a53860523f63381fdef5be0834403
Steve Brasier <steveb@stackhpc.com>
2019-10-31 15:26:55 +0000

    add docstrings etc

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,16 @@
+""" Print the grafana dashboard URL for a job on stdout.
+
+    Use in an sbatch script which contains something like:
+    
+    job.sh:
+        startstamp="$(date)"
+        $CMD
+        endstamp="$(date)"
+        echo "dashboard url for this job:" $(python dashboard.py "$startstamp" "$endstamp")
+       
+    Works on python 2.7 and 3.7.4 at least.
+"""
+
 import time, os, sys, subprocess
 
 def datestr_to_ns(s):
@@ -28,3 +41,6 @@ def get_dashboard_url(start, end, pre=30, post=30):
    
 if __name__ == '__main__':
     print(get_dashboard_url(*sys.argv[1:]))
+    
+    # tests (budget!)
+    #assert get_dashboard_url("Wed Oct 30 11:55:13 GMT 2019" "Wed Oct 30 12:03:02 GMT 2019") == "http://10.60.253.1:3000/dashboard/db/openhpc-compute?refresh=30s&orgId=4&from=1572436483000&to=1572437012000"
