87fddc29526dc7afe0c3ec0dd8f52eabf1b4750c
Steve Brasier <steveb@stackhpc.com>
2019-10-31 16:01:33 +0000

    add host filtering

--- dashboard.py
+++ dashboard.py
@@ -23,22 +23,34 @@ def datestr_to_ns(s):
     
     seconds_since_epoch = int(subprocess.check_output(('date', '+%s', '-d', s), universal_newlines=True).strip())
     return seconds_since_epoch * 1000
-    
 
-def get_dashboard_url(start, end, pre=30, post=30):
+def expand_hosts(hostlist):
+    """ Expand a slurm hostlist like "openhpc-compute-[12-13]" into a list of strs.
+    
+        Requires `scontrol` to be available.
+    """
+    hostnames = subprocess.check_output(('scontrol', 'show', 'hostnames', hostlist), universal_newlines=True).split('\n')
+    return hostnames
+
+def get_dashboard_url(start, end, hosts=None, pre=30, post=30):
     """ Get the grafana dashboard url for a given time range.
     
         start, end: strs from `date` - tries to be broad in format it accepts
+        hosts: str giving slurm hostnames, e.g. "openhpc-compute-[12-13]"
         pre, post: number of seconds to pad timespan by
         
         Returns a str.
     """
     start_ns = datestr_to_ns(start) - pre *1000
     end_ns = datestr_to_ns(end) + post * 1000
-    
     url = 'http://10.60.253.1:3000/dashboard/db/openhpc-compute?refresh=30s&orgId=4&from={start}&to={end}'.format(start=start_ns, end=end_ns)
+    if not hosts:
+        return url
+    hostnames = expand_hosts(hosts)
+    for host in hostnames:
+        url += '&var-hostname={host}.novalocal'.format(host=host)
     return url
-   
+    
 if __name__ == '__main__':
     print(get_dashboard_url(*sys.argv[1:]))
     
